3f0639c206be0ed1cd249afeb7a10c5f2d8db8c3
Noor-Shinh <71406898+Noor-Shinh@users.noreply.github.com>
2020-10-13 18:39:42 -0700

    Update assignment_test.py

--- assignment_test.py
+++ assignment_test.py
@@ -1,33 +1,76 @@
-#! python3
+import math
 
-import assignment
+def tempConversion(a,b="Celcius"):
+    if b=="Celcius":
+        x=a*(9/5)+32
+        x=round(x,1)
+        return x
+    elif b=="F":
+        x=(a-32)*5/9
+        x=round(x,1)
+        return x
 
-def test1():
-  assert assignment.function() == expected output
+def factorPair():
+    factors=[b]
+    for i in range(1,a+1):
+        if b*i==a:
+            factors.append(i)
+            factors.sort()
+            return factors
 
-def test2():
-  assert "needle" in assignment.function()
-  assert "needle2" in assignment.function(),"error Message"
-#!python3
-import assignment
+def toRadians(y):
+    x=(y*math.pi/180)   
+    return x
 
-def test1():
-  x = assignment.toRadians(30)
-  assert round(x,4) == 0.5236
+def cosineLaw(a,b,c,oppositeside=True):
+    if oppositeside==True:
+        stepc1=math.pow(a,2)+math.pow(b,2)
+        stepc2=(2*a*b)
+        stepc3=toRadians(c)
+        stepc4=math.cos(stepc3)
+        stepc5=(stepc2*stepc4)
+        stepc6=(stepc1)-(stepc5)
+        stepc7=math.sqrt(stepc6)
+        return stepc7
+    elif oppositeside==False:
+        d0=toRadians(c)
+        print(d0)
+        d1=math.sin(d0)*a/b
+        print(d1)
+        d2=math.asin(d1)
+        print(d2)
+        dc=d2*180/math.pi
+        d3=180-dc-c
+        print(d3)
+        d4=toRadians(d3)
+        print(d4)
+        d5=(math.sin(d4)*180/math.pi)*b/(math.sin(d0)*180/math.pi)
+        return d5
 
-def test2():
-  x = assignment.solution([-8.9, 5.3])
-  assert x == 5.3
 
-def test3():
-  x1,x2 = assignment.quadratic(3,5,-8)
-  assert round(x1,2) == -2.67
-  assert round(x2,2) == 1
 
-def test4():
-  answer = assignment.cosineLaw(6,9,34)
-  assert round(answer,1) == 5.2
 
-def test5():
-  answer = assignment.cosineLaw(10,3,50,oppositeSide=False)
-  assert round(answer,1) == 11.7 
+
+
+
+
+
+def solution(numbers):
+    x=numbers[1]
+    return x 
+
+def quadratic(a,b,c):
+    stepx1=(b*-1)
+    stepx2=math.pow(b,2)-(4*a*c)
+    stepx2=math.sqrt(stepx2)
+    stepx3=(stepx1)+(stepx2) 
+    stepx4=(stepx3)/(2*a)
+    stepy1=(b*-1)
+    stepy2=math.pow(b,2)-(4*a*c)
+    stepy2=math.sqrt(stepy2)
+    stepy3=(stepy1)-(stepy2) 
+    stepy4=(stepy3)/(2*a)
+    numlist=[stepy4,stepx4]
+    numlist.sort()
+    return numlist
+
